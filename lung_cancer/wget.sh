1f7d5a434923ef878aa3397d687d7e3d160d415f
peterthorpe5 <pjt6@st-andrews.ac.uk>
2021-02-14 12:25:51 +0000

    back up

---
+++ lung_cancer/wget.sh
@@ -0,0 +1,12 @@
+cd /storage/home/users/pjt6/docker/seq2hla/
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR948/SRR948990/SRR948990_*.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR650/008/SRR6504958/SRR6504958_*.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR650/008/SRR6504958/SRR6504958_2.fastq.gz
+SRR8635691_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR863/001/SRR8635691/SRR8635691_*.fastq.gz 
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR863/001/SRR8635691/SRR8635691_2.fastq.gz 
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR863/005/SRR8636145/SRR8636145_*.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR271/003/ERR2712993/ERR2712993_*.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR271/003/ERR2712993/ERR2712993_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR867/002/SRR8670702/SRR8670702_*.fastq.gz
---
+++ optitype/optitype.sh
@@ -0,0 +1,87 @@
+cd /storage/home/users/pjt6/docker/optitype
+
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR925703_1.fastq.gz SRR925703_2.fastq.gz -o HCC_1395_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR925708_1.fastq.gz SRR925708_2.fastq.gz -o HCC_1806_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR925727_1.fastq.gz SRR925727_2.fastq.gz -o MDAMB361_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR1283038_1.fastq.gz SRR1283038_2.fastq.gz -o MDAMB453_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR925710_1.fastq.gz SRR925710_2.fastq.gz -o HCC1954_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR925723_1.fastq.gz SRR925723_2.fastq.gz -o MCF7_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR925726_1.fastq.gz SRR925726_2.fastq.gz -o MDAMB231_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR2532369_1.fastq.gz SRR2532369_2.fastq.gz -o MDAMB415_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR925729_1.fastq.gz SRR925729_2.fastq.gz -o SKBR3_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR925736_1.fastq.gz SRR925736_2.fastq.gz -o T47D_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR925740_1.fastq.gz SRR925740_2.fastq.gz -o ZR751_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR925742_1.fastq.gz SRR925742_2.fastq.gz -o ZR75B_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR925694_1.fastq.gz SRR925694_2.fastq.gz -o AU565_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SRR925709_1.fastq.gz SRR925709_2.fastq.gz -o HCC1937_raw_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input HCC_1395_paired_1.fq.gz HCC_1395_paired_2.fq.gz -o HCC_1395_trimmed_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input HCC_1806_paired_1.fq.gz HCC_1806_paired_2.fq.gz -o HCC_1806_trimmed_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input MDAMB361_paired_1.fq.gz MDAMB361_paired_2.fq.gz -o MDAMB361_trimmed_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input MDAMB453_paired_1.fq.gz MDAMB453_paired_2.fq.gz -o MDAMB453_trimmed_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input HCC1954_paired_1.fq.gz HCC1954_paired_2.fq.gz -o HCC1954_trimmed_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input MCF7_paired_1.fq.gz MCF7_paired_2.fq.gz -o MCF7_trimmed_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input MDAMB231_paired_1.fq.gz MDAMB231_paired_2.fq.gz -o MDAMB231_trimmed_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input MDAMB415_paired_1.fq.gz MDAMB415_paired_2.fq.gz -o MDAMB415_trimmed_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input SKBR3_paired_1.fq.gz SKBR3_paired_2.fq.gz -o SKBR3_trimmed_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input T47D_paired_1.fq.gz T47D_paired_2.fq.gz -o T47D_trimmed_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input ZR751_paired_1.fq.gz ZR751_paired_2.fq.gz -o ZR751_trimmed_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input ZR75B_paired_1.fq.gz ZR75B_paired_2.fq.gz -o ZR75B_trimmed_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input AU565_paired_1.fq.gz AU565_paired_2.fq.gz -o AU565_trimmed_optitype -v
+
+
+singularity run /storage/home/users/pjt6/docker/optitype/optitype_latest.sif --rna --input HCC1937_paired_1.fq.gz HCC1937_paired_2.fq.gz -o HCC1937_trimmed_optitype -v
+
